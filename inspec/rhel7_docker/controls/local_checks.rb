c1bb3d437537a097a928f3ab5c3b58da3268c712
Steve Bonds <sbonds@gmail.com>
2019-03-19 11:39:42 -0700

    Expand checks to ansible and java

--- inspec/rhel7_docker/controls/local_checks.rb
+++ inspec/rhel7_docker/controls/local_checks.rb
@@ -3,10 +3,18 @@
 
 title 'Checks of the local host for running Jenkins in Docker'
 
-control 'docker_installed' do
-  title 'Check that Docker is installed'
+control 'software_installed' do
+  title 'Check that needed software is installed'
   describe package('docker') do
     it { should be_installed }
     its ('version') { should cmp >= '1.13' }
+  end
+  describe package('ansible') do
+    it { should be_installed }
+    its ('version') { should cmp >= '2.7.8' }
+  end
+  describe package('java') do
+    it { should be_installed }
+    its ('version') { should cmp >= '1.8.0' }
   end
 end
