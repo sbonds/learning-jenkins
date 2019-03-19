e405c8a2a48c35ba1ede633f9c83768e6c70bed8
Steve Bonds <sbonds@gmail.com>
2019-03-19 13:16:25 -0700

    Leave firewall checks out due to polkit bugs

--- inspec/rhel7_docker/controls/local_checks.rb
+++ inspec/rhel7_docker/controls/local_checks.rb
@@ -41,9 +41,14 @@ end
 # Arguably the default doesn't really need to be public, but this lets
 # me be sure which zone to check as Inspec doesn't seem to have a way
 # to refer to the default zone for later checks.
+=begin
 describe firewalld do
+  title "Check that firewall ports are open to the Jenkins container from a local subnet (BUGGY: requires root)"
+  # https://github.com/firewalld/firewalld/issues/111
+  # https://bugzilla.redhat.com/show_bug.cgi?id=1375655
   it { should be_running }
   its('default_zone') { should eq 'public' }
   it { should have_rule_enabled('family=ipv4 source address=' + subnet + ' port port=8080 protocol=tcp accept', 'public') }
   it { should have_rule_enabled('family=ipv4 source address=' + subnet + ' port port=50000 protocol=tcp accept', 'public') }
 end
+=end
